96a751d52023622ffd1a05c238092577fa133979
AlphaT7 <7742643+AlphaT7@users.noreply.github.com>
2022-04-25 21:17:01 +0000

    progress

--- javascript.js
+++ javascript.js
@@ -4,83 +4,108 @@ const log = console.log.bind(console);
 
 const a = {
   data: `<tr>
-          <td><input type="text" placeholder="data" /></td>
+          <td><label for="elData">Data</td>
+          <td><input id="elData" data-attrtype="data" type="text" placeholder="data" name="attributeValue"/></td>
         </tr>`,
   class: `<tr>
-            <td><input type="text" placeholder="class" /></td>
+            <td><label for="elClass">Class</label></td>
+            <td><input id="elClass" data-attrtype="text" type="text" placeholder="class" name="attributeValue"/></td>
           </tr>`,
   id: `<tr>
-          <td><input type="text" placeholder="id" /></td>
+          <td><label for="elId">Id</label></td>
+          <td><input id="elId" data-attrtype="id" type="text" placeholder="id" name="attributeValue"/></td>
         </tr>`,
   name: `<tr>
-          <td><input type="text" placeholder="name" /></td>
+          <td><label for="elName">Name</label></td>
+          <td><input id="elName" data-attrtype="name" type="text" placeholder="name" name="attributeValue"/></td>
         </tr>`,
-  "option-values": `<tr>
-                      <td><textarea type="text" placeholder="option-value1,&#13;option-value2,&#13;option-value3,&#13;option-value4,&#13;etc..." rows="5"></textarea></td>
-                    </tr>`,
+  option: `<tr>
+            <td><label for="elOption">Option</label></td>
+            <td><textarea id="elOption" data-attrtype="option" type="text" placeholder="name:value1,&#13;name:value2,&#13;name:value3,&#13;name:value4,&#13;etc..." rows="5" name="attributeValue"></textarea></td>
+          </tr>`,
   value: `<tr>
-          <td><input type="text" placeholder="value" /></td>
-        </tr>`,
+             <td><label for="elValue">Value</label></td>
+             <td><input id="elValue" data-attrtype="value" type="text" placeholder="value" name="attributeValue"/></td>
+          </tr>`,
   min: `<tr>
-          <td><input type="text" placeholder="min" /></td>
+          <td><label for="elMin">Min</label></td>
+          <td><input id="elMin" data-attrtype="min" type="text" placeholder="min" name="attributeValue"></td>
         </tr>`,
   max: `<tr>
-          <td><input type="text" placeholder="max" /></td>
+          <td><label for="elMax">Max</label></td>
+          <td><input id="elMax" data-attrtype="max" type="text" placeholder="max" name="attributeValue"></td>
         </tr>`,
   step: `<tr>
-          <td><input type="text" placeholder="step" /></td>
+          <td><label for="elStep">Step</label></td>
+          <td><input id="elStep" data-attrtype="step" type="text" placeholder="step" name="attributeValue"></td>
         </tr>`,
   pattern: `<tr>
-              <td><input type="text" placeholder="pattern" /></td>
+              <td><label for="elPattern">Pattern</label></td>
+              <td><input id="elPattern" data-attrtype="pattern" type="text" placeholder="pattern" name="attributeValue"></td>
             </tr>`,
   accept: `<tr>
-              <td><input type="text" placeholder="accept" /></td>
+              <td><label for="elAccept">Accept</label></td>
+              <td><input id="elAccept" data-attrtype="accept" type="text" placeholder="accept" name="attributeValue"></td>
             </tr>`,
   rows: `<tr>
-              <td><input type="text" placeholder="rows" /></td>
-            </tr>`,
+            <td><label for="elRows">Rows</label></td>
+            <td><input id="elRows" data-attrtype="rows" type="text" placeholder="rows" name="attributeValue"></td>
+        </tr>`,
   cols: `<tr>
-              <td><input type="text" placeholder="cols" /></td>
-            </tr>`,
+            <td><label for="elCols">Cols</label></td>
+            <td><input id="elCols" data-attrtype="cols" type="text" placeholder="cols" name="attributeValue"></td>
+          </tr>`,
   wrap: `<tr>
-              <td><input type="text" placeholder="wrap" /></td>
-            </tr>`,
+            <td><label for="elWrap">Wrap</label></td>
+            <td><input id="elWrap" data-attrtype="wrap" type="text" placeholder="wrap" name="attributeValue"></td>
+        </tr>`,
   multiple: `<tr>
+              <td><label for="elMultiple">Multiple</label></td> 
               <td>
-                <select>
+                <select id="elMultiple" data-attrtype="multiple" name="attributeValue">
                   <option value="true">Multiple = True</option>
                   <option value="false" selected>Multiple = False</option>
                 </select>
               </td>
             </tr>`,
   minlength: `<tr>
-                <td><input type="text" placeholder="minlength" /></td>
+                <td><label for="elMinLength">MinLength</label></td>
+                <td><input id="elMinLength" data-attrtype="minlength" type="text" placeholder="minlength" name="attributeValue"></td>
               </tr>`,
   maxlength: `<tr>
-                <td><input type="text" placeholder="maxlength" /></td>
+                <td><label for="elMaxLength">MaxLength</label></td>
+                <td><input id="elMaxLength" data-attrtype="maxlength" type="text" placeholder="maxlength" name="attributeValue"></td>
               </tr>`,
   required: `<tr>
+              <td><label for="elRequired">Required</label></td>
               <td>
-                <select>
+                <select id="elRequired" data-attrtype="required" name="attributeValue">
                   <option value="true">Required = True</option>
                   <option value="false" selected>Required = False</option>
                 </select>
               </td>
             </tr>`,
   checked: `<tr>
+              <td><label for="elChecked">Checked</label></td>
               <td>
-                <select>
+                <select id="elChecked" data-attrtype="checked" name="attributeValue">
                   <option value="true">Checked = True</option>
                   <option value="false" selected>Checked = False</option>
                 </select>
               </td>
             </tr>`,
   list: `<tr>
-          <td><input type="text" placeholder="list" /></td>
+          <td><label for="elList">List</label></td>
+          <td><input id="elList" data-attrtype="list" type="text" placeholder="list" name="attributeValue"></td>
         </tr>`,
+  labelname: `<tr>
+                <td><label for="elLabelName">LabelName</label></td>
+                <td><input id="elLabelName" data-attrtype="labelname" type="text" placeholder="labelname" name="attributeValue"></td>
+              </tr>`,
   formenctype: `<tr>
+                  <td><label for="elFormEnctype">FormEnctype</label></td>
                   <td>
-                    <select>
+                    <select id="elFormEnctype" data-attrtype="formenctype" name="attributeValue">
                       <option value="" selected>Choose Formenctype</option>
                       <option value="application/x-www-form-urlencoded">application/x-www-form-urlencoded</option>
                       <option value="multipart/form-data">multipart/form-data</option>
@@ -89,8 +114,9 @@ const a = {
                   </td>
                 </tr>`,
   formmethod: `<tr>
+                  <td><label for="elFormMethod">FormMethod</label></td>
                   <td>
-                    <select>
+                    <select id="elFormMethod" data-attrtype="formmethod" name="attributeValue">
                       <option value="" selected>Choose Formmethod</option>
                       <option value="get">get</option>
                       <option value="post">post</option>
@@ -100,19 +126,10 @@ const a = {
 };
 
 const elementDecisionTree = {
-  button: [a.id, a.class, a.name, a.data, a.value],
-  checkbox: [a.id, a.class, a.name, a.data, a.checked],
-  color: [a.id, a.class, a.name, a.data, a.value, a.required],
-  "datalist-input": [
-    a.id,
-    a.class,
-    a.name,
-    a.data,
-    a.list,
-    a.value,
-    a.required,
-  ],
-  datalist: [a.id, a["option-values"]],
+  button: [a.id, a.class, a.name, a.data, a.value, a.labelname],
+  checkbox: [a.id, a.class, a.name, a.data, a.checked, a.labelname],
+  color: [a.id, a.class, a.name, a.data, a.value, a.labelname, a.required],
+  datalist: [a.id, a.option],
   date: [
     a.id,
     a.class,
@@ -122,6 +139,7 @@ const elementDecisionTree = {
     a.min,
     a.max,
     a.pattern,
+    a.labelname,
     a.required,
   ],
   "datetime-local": [
@@ -133,9 +151,19 @@ const elementDecisionTree = {
     a.min,
     a.max,
     a.step,
+    a.labelname,
     a.required,
   ],
-  email: [a.id, a.class, a.name, a.data, a.value, a.pattern, a.required],
+  email: [
+    a.id,
+    a.class,
+    a.name,
+    a.data,
+    a.value,
+    a.pattern,
+    a.labelname,
+    a.required,
+  ],
   file: [
     a.id,
     a.class,
@@ -144,6 +172,7 @@ const elementDecisionTree = {
     a.value,
     a.accept,
     a.multiple,
+    a.labelname,
     a.required,
   ],
   hidden: [a.id, a.name, a.data, a.value],
@@ -158,6 +187,7 @@ const elementDecisionTree = {
     a.list,
     a.step,
     a.pattern,
+    a.labelname,
     a.required,
   ],
   number: [
@@ -171,6 +201,7 @@ const elementDecisionTree = {
     a.step,
     a.pattern,
     a.list,
+    a.labelname,
     a.required,
   ],
   password: [
@@ -181,11 +212,23 @@ const elementDecisionTree = {
     a.min,
     a.max,
     a.pattern,
+    a.labelname,
     a.required,
   ],
-  radio: [a.id, a.class, a.name, a.data, a.value, a.checked],
-  range: [a.id, a.class, a.name, a.data, a.value, a.min, a.max, a.step, a.list],
-  reset: [a.id, a.class, a.name, a.data, a.value],
+  radio: [a.id, a.class, a.name, a.data, a.value, a.labelname, a.checked],
+  range: [
+    a.id,
+    a.class,
+    a.name,
+    a.data,
+    a.value,
+    a.min,
+    a.max,
+    a.step,
+    a.labelname,
+    a.list,
+  ],
+  reset: [a.id, a.class, a.name, a.data, a.labelname, a.value],
   search: [
     a.id,
     a.class,
@@ -196,6 +239,7 @@ const elementDecisionTree = {
     a.minlength,
     a.maxlength,
     a.pattern,
+    a.labelname,
     a.required,
   ],
   select: [
@@ -205,9 +249,19 @@ const elementDecisionTree = {
     a.data,
     a.value,
     a.multiple,
-    a["option-values"],
+    a.labelname,
+    a.option,
   ],
-  submit: [a.id, a.class, a.name, a.data, a.value, a.formmethod, a.formenctype],
+  submit: [
+    a.id,
+    a.class,
+    a.name,
+    a.data,
+    a.value,
+    a.formmethod,
+    a.labelname,
+    a.formenctype,
+  ],
   tel: [
     a.id,
     a.class,
@@ -218,6 +272,7 @@ const elementDecisionTree = {
     a.maxlength,
     a.minlength,
     a.pattern,
+    a.labelname,
     a.required,
   ],
   text: [
@@ -230,9 +285,20 @@ const elementDecisionTree = {
     a.minlength,
     a.maxlength,
     a.pattern,
+    a.labelname,
     a.required,
   ],
-  textarea: [a.id, a.class, a.name, a.data, a.rows, a.cols, a.wrap, a.required],
+  textarea: [
+    a.id,
+    a.class,
+    a.name,
+    a.data,
+    a.rows,
+    a.cols,
+    a.wrap,
+    a.labelname,
+    a.required,
+  ],
   time: [
     a.id,
     a.class,
@@ -243,6 +309,7 @@ const elementDecisionTree = {
     a.minlength,
     a.maxlength,
     a.pattern,
+    a.labelname,
     a.required,
   ],
   url: [
@@ -255,6 +322,7 @@ const elementDecisionTree = {
     a.list,
     a.minlength,
     a.maxlength,
+    a.labelname,
     a.required,
   ],
   week: [
@@ -266,6 +334,7 @@ const elementDecisionTree = {
     a.min,
     a.max,
     a.list,
+    a.labelname,
     a.required,
   ],
 };
@@ -276,27 +345,58 @@ document.querySelectorAll(".pushElement").forEach((button) => {
   });
 });
 
-document
-  .querySelector("#addElementPanel")
-  .addEventListener("click", function () {
-    document.querySelector("#addElementPanel").style.display = "none";
-    document.querySelector("#addElementTable").style.display = "none";
-    document.querySelector("#elements_table").style.filter = "blur(0px)";
+function closeAddElement() {
+  document.querySelector("#addElementBG").style.display = "none";
+  document.querySelector("#addElementTable").style.display = "none";
+  document.querySelector("#elements_table").style.filter = "blur(0px)";
+}
+
+function createEl() {
+  let attributeArray = Array.from(
+    document.querySelectorAll("[name='attributeValue']")
+  );
+  let el = attributeArray.filter((el) => {
+    return el.type == "hidden";
   });
+  let elType = el[0].dataset.eltype;
+  let isInput = el[0].dataset.isinput;
+
+  if (isInput) {
+    let element = `
+      <input type="${elType}"/>
+    `;
+    log(element);
+  } else {
+    alert("Not an Input");
+  }
+}
 
 function chooseElement(e) {
-  let title = `<tr class="panelTitle">
-                  <td>${e.target.dataset.eltype}</td>
-                </tr>`;
+  let title = `<thead class="panelTitle">
+                  <th colspan="2">
+                      ${e.target.dataset.eltype}
+                      <input type="hidden" data-eltype="${e.target.dataset.eltype}" data-isinput="${e.target.dataset.isinput})" name="attributeValue" />
+                  </th>
+                </thead>
+                <tbody>`;
 
   let elementAttributes = elementDecisionTree[e.target.dataset.eltype].join("");
 
   let submitButton = `<tr>
-                        <td><input type="button" value="Add" /></td>
-                      </tr>`;
+                        <td colspan="2">
+                            <input
+                              type="button"
+                              value="Add ${e.target.dataset.eltype}"
+                              id="createEl"
+                              onclick="createEl();"
+                            />
+                            <input type="button" value="Cancel" id="cancelEl" onclick="closeAddElement()"/>
+                          </td>
+                      </tr>
+                      </tbody>`;
 
-  document.querySelector("#addElementPanel").style.display = "block";
-  document.querySelector("#addElementTable").style.display = "block";
+  document.querySelector("#addElementBG").style.display = "block";
+  document.querySelector("#addElementTable").style.display = "table";
   document.querySelector("#elements_table").style.filter = "blur(5px)";
   document.querySelector("#addElementTable").innerHTML =
     title + elementAttributes + submitButton;
